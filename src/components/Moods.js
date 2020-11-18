e2685d10476f8c64ad61c725d4ab44c4b19f0b24
Lukecp <lukecp@pm.me>
2020-11-17 22:13:23 -0500

    first two components completed

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -4,11 +4,9 @@ MOODS Instructions
 Watch this short video:
 https://tk-assets.lambdaschool.com/fe491c4f-395b-425c-b62e-3fbebf3cbb3f_moods-clip.gif
 
-How many slices of state do you think are necessary to act as "sources of truth" for all
-the things that change in this widget? Give it some thought before continuing reading!
+How many slices of state do you think are necessary to act as "sources of truth" for all the things that change in this widget? Give it some thought before continuing reading!
 
-Some might say 3 different states: one per mood (happy, sad, and uncertain).
-But a single slice of state is more than enough to keep track of all these moods!
+Some might say 3 different states: one per mood (happy, sad, and uncertain). But a single slice of state is more than enough to keep track of all these moods!
 
 STEP 0:
   Study the component below, and import the state hook.
@@ -28,35 +26,35 @@ STEPS 4, 5, 6:
   Inside these click handlers set the correct mood, using 'setMood' and the variables below the imports.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 const initialMood = 'Not sure how I feel';
 const happyMood = 'Quite happy!';
 const sadMood = 'Rather sad';
 
 export default function Moods() {
-  /* STEP 1 */
+  const [mood, setMood] = useState(initialMood);
 
   const makeHappy = () => {
-    /* STEP 4 */
+    setMood(happyMood);
   };
   const makeSad = () => {
-    /* STEP 5 */
+    setMood(sadMood);
   };
   const reset = () => {
-    /* STEP 6 */
+    setMood(initialMood);
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'crimson', /* STEP 2 */
+    color: mood == happyMood ? 'royalblue' : 'crimson', /* STEP 2 */
   };
 
   return (
     <div className='widget-moods container'>
       <h2>Moods</h2>
-      <div id='mood' style={style}>Not sure how I feel</div> {/* STEP 3 */}
+      <div id='mood' style={style}>{mood}</div> {/* STEP 3 */}
       <div>
         <button id='makeHappy' onClick={makeHappy}>Make Happy</button>
         <button id='makeSad' onClick={makeSad}>Make Sad</button>
